8219004d3c4a3ebecec8667913169a4671cefc86
Jurgita Kaziuniene <jurgita.dulkyte@gmail.com>
2024-05-07 21:56:04 +0300

    added components

---
+++ FrontEnd/src/components/Layouts/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="footer-wrap">
+      © 2024 CallInEventSignup. All rights reserved.
+    </div>
+  )
+}
+
+export default Footer
---
+++ FrontEnd/src/pages/Attendees/Attendees.jsx
@@ -0,0 +1,15 @@
+import Footer from "../../components/Layouts/Footer";
+
+const Attendees = () => {
+    return (
+        <main>
+            <Footer>
+            <div className="content">
+                home
+            </div>
+           </Footer>
+        </main>
+    );
+  };
+  
+  export default Attendees;
